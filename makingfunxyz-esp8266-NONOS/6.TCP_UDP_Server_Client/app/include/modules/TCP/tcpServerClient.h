56c981e992908e91ca20fca8f13f942b92d08351
imliubo <imliubo@makingfun.xyz>
2018-12-04 23:53:34 +0800

    add TCP UDP Server Client example

---
+++ makingfunxyz-esp8266-NONOS/6.TCP_UDP_Server_Client/app/include/modules/TCP/tcpServerClient.h
@@ -0,0 +1,52 @@
+/*MIT License
+Copyright (c) 2018 imliubo
+Github  https://github.com/imliubo
+Website https://www.makingfun.xyz
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+#ifndef TCPSERVERCLIENT_H_
+#define TCPSERVERCLIENT_H_
+
+#include "driver/uart.h"
+#include "osapi.h"
+#include "espconn.h"
+#include "mem.h"
+
+/**
+ * TCP Client functions
+ */
+
+void tcp_client_sent_cb(void *arg);
+void tcp_client_recv_cb(void *arg,char *pdata,unsigned short lenth);
+void tcp_client_recon_cb(void *arg,sint8 error);
+void tcp_client_discon_cb(void *arg);
+void tcp_client_connect_cb(void *arg);
+void tcp_client_send_data(void *arg,char *pdata,unsigned short lenth);
+void tcp_client_init(void);
+
+/**
+ * TCP Server functions
+ */
+void tcp_server_sent_cb(void *arg);
+void tcp_server_recv_cb(void *arg,char *pdata,unsigned short len);
+void tcp_server_recon_cb(void *arg,sint8 error);
+void tcp_server_discon_cb(void *arg);
+void tcp_server_listen_cb(void *arg);
+void tcp_server_send_data(void *arg,char *pdata,unsigned short lenth);
+void tcp_server_init(void);
+
+#endif
