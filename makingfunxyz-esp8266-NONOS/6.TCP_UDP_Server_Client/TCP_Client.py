2df49fe8d992f86bef5473fb40c0fd012c843125
imliubo <imliubo@makingfun.xyz>
2018-12-09 23:16:47 +0800

    fix some code

--- makingfunxyz-esp8266-NONOS/6.TCP_UDP_Server_Client/TCP_Client.py
+++ makingfunxyz-esp8266-NONOS/6.TCP_UDP_Server_Client/TCP_Client.py
@@ -1,24 +1,20 @@
 from socket import *
-
-TCP_SERVER_HOST = '192.168.4.1'
+ 
+TCP_SERVER_HOST = '192.168.0.107'
 TCP_SERVER_PORT = 6666
 TCP_SERVER_ADDR = (TCP_SERVER_HOST,TCP_SERVER_PORT)
 
-i = 1
+i = 0
 
 tcpClient = socket(AF_INET,SOCK_STREAM)
 tcpClient.connect(TCP_SERVER_ADDR)
-
-print("PC Client connect to ESP8266 server!")
-while i:
-    #recvData = tcpClient.recv(1024).decode('utf-8')
-    #if not recvData:
-    #    break
-    #print(recvData)
-    #i = i + 1
-    i = i - 1
-    tcpClient.send(("{\"SSID\":\"CMCC-Master\",\"PWD\":\"zxcvbnmlk\"}").encode())
-
+ 
+while True:
+    recvData = tcpClient.recv(1024).decode('utf-8')
+    if not recvData:
+        break
+    print(recvData)
+    i = i + 1
+    tcpClient.send(("hi this is PC client! message num " + str(i)).encode())
     
-# tcpClient.close()
-
+tcpClient.close()
