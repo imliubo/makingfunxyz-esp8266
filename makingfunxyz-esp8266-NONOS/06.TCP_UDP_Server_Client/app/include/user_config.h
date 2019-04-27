1267bb81a1886bf59926657a3a261285ef7799f0
imliubo <imliubo@makingfun.xyz>
2019-04-27 10:49:04 +0800

    change some file,delete personal info

--- makingfunxyz-esp8266-NONOS/06.TCP_UDP_Server_Client/app/include/user_config.h
+++ makingfunxyz-esp8266-NONOS/06.TCP_UDP_Server_Client/app/include/user_config.h
@@ -27,20 +27,21 @@
 
 #define USE_OPTIMIZE_PRINTF
 
+//At the same time, only one can be 1
 #define TCP_CLIENT 0
 #define TCP_SERVER 0
-
 #define UDP_TEST   1
 
-#define STA_SSID "CMCC-7uHL"//your wifi SSID
-#define STA_PASS "zxcvbnmlk"//your wifi password
+#define STA_SSID "XXXXXXXXX"//your wifi SSID
+#define STA_PASS "xxxxxxxxx"//your wifi password
 #define STA_TYPE AUTH_WPA2_PSK
 
-#define TCP_SERVER_IP    "192.168.1.10"//TCP server IP,it is according your PC IP
+#define TCP_SERVER_IP    "192.168.1.10"//TCP server IP,it is according your TCP server PC or device IP
 #define TCP_SERVER_PORT  6666
 #define TCP_LOCAL_PORT	 8888
 
-#define UDP_REMOTE_IP    "192.168.1.10"//UDP server IP,it is according your PC IP
+#define UDP_REMOTE_IP    "192.168.1.10"//UDP remote IP,With the connectionless UDP protocol, 
+				       //I don't think we should divide the server and the client.
 #define UDP_REMOTE_PORT  6666
 #define UDP_LOCAL_PORT	 8888
 
